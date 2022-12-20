4c4745d0b401d6ec4c5e913fd92783b9c6cfa964
dohyu <dohyunre702@gmail.com>
2022-12-20 13:15:03 +0900

    [add:get] HelloController - get요청 추가

---
+++ src/main/java/com/example/dhlee/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.example.dhlee.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello, 200";
+    }
+}
