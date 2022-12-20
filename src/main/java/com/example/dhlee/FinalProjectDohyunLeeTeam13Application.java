2b093e76fe7b785624b380ff1bff8d802152dd33
dohyu <dohyunre702@gmail.com>
2022-12-20 11:26:48 +0900

    Initial commit

---
+++ src/main/java/com/example/dhlee/FinalProjectDohyunLeeTeam13Application.java
@@ -0,0 +1,13 @@
+package com.example.dhlee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalProjectDohyunLeeTeam13Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinalProjectDohyunLeeTeam13Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dhlee/FinalProjectDohyunLeeTeam13ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dhlee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalProjectDohyunLeeTeam13ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
