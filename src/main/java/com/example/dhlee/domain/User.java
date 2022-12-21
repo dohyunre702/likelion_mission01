d542261d7537f687ba0b78cffe893bc74e888576
dohyu <dohyunre702@gmail.com>
2022-12-21 11:45:45 +0900

    [updeate] userEntity 수정

--- src/main/java/com/example/dhlee/domain/User.java
+++ src/main/java/com/example/dhlee/domain/User.java
@@ -2,6 +2,7 @@ package com.example.dhlee.domain;
 
 import lombok.*;
 import javax.persistence.*;
+import java.time.LocalDateTime;
 
 //user 테이블과 연결하는 클래스
 @Entity
@@ -14,16 +15,30 @@ import javax.persistence.*;
 public class User {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //인덱스값
     private Long id;
 
-    @Column(nullable = false, unique = true)
-    private String userName;
+    @Column(nullable = false, unique = true) //token 생성 위한 유저id
+    private String uid;
 
-    @Column(nullable = false)
+    @Column(nullable = false) //비밀번호
     private String password;
 
-    @Column(nullable = false)
+    @Column(nullable = false, unique = true) //유저닉네임
+    private String userName;
+
+    @Column(nullable = false) //이메일주소
     private String emailAddress;
+
+    /*
+    @Column
+    private LocalDateTime registeredAt;
+
+    @Column
+    private LocalDateTime updatedAt;
+
+    @Column
+    private LocalDateTime deletedAt;
+     */
 
 }
