516fce82fa3f457f9cf2818edbc03b647dc80ff8
dohyu <dohyunre702@gmail.com>
2022-12-22 12:48:41 +0900

    [update] UserServiceImpl - UserLoginRequest와 구분하기 위해 request 매개변수명 변경

--- src/main/java/com/example/dhlee/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/dhlee/service/impl/UserServiceImpl.java
@@ -4,6 +4,7 @@ import com.example.dhlee.config.security.JwtTokenProvider;
 import com.example.dhlee.domain.User;
 import com.example.dhlee.domain.dto.UserDto;
 import com.example.dhlee.domain.dto.UserJoinRequest;
+import com.example.dhlee.domain.dto.UserLoginRequest;
 import com.example.dhlee.exception.ErrorCode;
 import com.example.dhlee.exception.SignException;
 import com.example.dhlee.repository.UserRepository;
@@ -20,16 +21,16 @@ public class UserServiceImpl {
     public JwtTokenProvider jwtTokenProvider;
 
     //회원가입
-    public UserDto join(UserJoinRequest request) {
+    public UserDto join(UserJoinRequest userJoinRequest) {
 
         //같은 이름이 있는 중복회원 => 가입 불가
-        userRepository.findByUsername(request.getUsername())
+        userRepository.findByUsername(userJoinRequest.getUsername())
                 .ifPresent(user -> {
-                    throw new SignException(ErrorCode.DUPLICATED_USER_NAME, String.format("username:%s", request.getUsername()));
+                    throw new SignException(ErrorCode.DUPLICATED_USER_NAME, String.format("username:%s", userJoinRequest.getUsername()));
                 });
 
         //중복 체크 통과 뒤 회원가입 = .save() > entity로 값이 저장되어 이동
-        User savedUser = userRepository.save(request.toEntity());
+        User savedUser = userRepository.save(userJoinRequest.toEntity());
 
         return UserDto.builder()
                 .id(savedUser.getId())
@@ -40,4 +41,6 @@ public class UserServiceImpl {
     }
 
 
+    public UserDto login(UserLoginRequest userLoginRequest) {
+    }
 }
