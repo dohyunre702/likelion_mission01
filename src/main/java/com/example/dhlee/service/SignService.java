f54e06b32881a14d220b2202cbade2db4b3ecddb
dohyu <dohyunre702@gmail.com>
2022-12-22 09:25:56 +0900

    [add] SignService

--- src/main/java/com/example/dhlee/service/SignService.java
+++ src/main/java/com/example/dhlee/service/SignService.java
@@ -3,7 +3,7 @@ package com.example.dhlee.service;
 import com.example.dhlee.domain.dto.*;
 
 public interface SignService {
-    UserJoinResponse signUp (UserJoinRequest userJoinRequest); //회원가입
+    UserDto join (UserJoinRequest userJoinRequest); //회원가입
     UserLoginResponse signIn (UserLoginRequest userLoginRequest); //로그인
 
 }
--- src/main/java/com/example/dhlee/service/impl/SignServiceImpl.java
+++ src/main/java/com/example/dhlee/service/impl/SignServiceImpl.java
@@ -4,30 +4,34 @@ import com.example.dhlee.config.security.JwtTokenProvider;
 import com.example.dhlee.domain.User;
 import com.example.dhlee.domain.dto.UserDto;
 import com.example.dhlee.domain.dto.UserJoinRequest;
+import com.example.dhlee.error.ErrorCode;
 import com.example.dhlee.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
+@RequiredArgsConstructor
 public class SignServiceImpl {
+
     public UserRepository userRepository;
     public JwtTokenProvider jwtTokenProvider;
-
     private UserDto userDto;
 
-    @Autowired
-    public SignServiceImpl(UserRepository userRepository, JwtTokenProvider jwtTokenProvider) {
+    public SignServiceImpl(UserRepository userRepository, JwtTokenProvider jwtTokenProvider, UserDto userDto) {
         this.userRepository = userRepository;
         this.jwtTokenProvider = jwtTokenProvider;
+        this.userDto = userDto;
     }
 
     //회원가입
-    public UserDto signUp(UserJoinRequest userJoinRequest) {
+    public UserDto join(UserJoinRequest userJoinRequest) {
 
-        //같은 이름이 있는 중복회원 X
+        //같은 이름이 있는 중복회원 => 가입 불가
         userRepository.findByUsername(userJoinRequest.getUsername())
                 .ifPresent(username -> {
-                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                    throw new IllegalStateException(String.valueOf(ErrorCode.DUPLICATED_USER_NAME));
                 });
-
 
         //중복 체크 통과 뒤 회원가입 = .save()
         User savedUser = userRepository.save(userJoinRequest.toEntity());
@@ -39,4 +43,6 @@ public class SignServiceImpl {
                 .emailAddress(savedUser.getEmailAddress())
                 .build();
     }
+
+
 }
