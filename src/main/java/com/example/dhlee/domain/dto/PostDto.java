8dcdd2cf3b5534ea1b5d462af593a47a07af41c9
dohyu <dohyunre702@gmail.com>
2022-12-23 14:55:43 +0900

    [update] BUILDER 추가

--- src/main/java/com/example/dhlee/domain/dto/PostDto.java
+++ src/main/java/com/example/dhlee/domain/dto/PostDto.java
@@ -1,5 +1,7 @@
 package com.example.dhlee.domain.dto;
 
+import com.example.dhlee.domain.Post;
+import com.example.dhlee.domain.User;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -17,4 +19,14 @@ public class PostDto {
     private String body;
     private LocalDateTime created_at;
     private LocalDateTime last_modified_at;
+
+    public Post toEntity() {
+        return Post.builder()
+                .id(this.id)
+                .title(this.title)
+                .body(this.body)
+                .created_at(this.created_at)
+                .last_modified_at(this.last_modified_at)
+                .build();
+    }
 }
