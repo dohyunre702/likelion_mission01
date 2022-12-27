a64751cb3092b0b6fe1420d4218c4eeb5a069885
dohyu <dohyunre702@gmail.com>
2022-12-27 13:20:01 +0900

    [update] 에러코드 정리

--- src/main/java/com/example/dhlee/exception/ErrorCode.java
+++ src/main/java/com/example/dhlee/exception/ErrorCode.java
@@ -7,7 +7,13 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 @Getter
 public enum ErrorCode {
-    DUPLICATED_USER_NAME(HttpStatus.CONFLICT, "중복된 회원입니다.");
+    DUPLICATED_USER_NAME(HttpStatus.CONFLICT, "UserName이 중복됩니다."),
+    USERNAME_NOT_FOUND(HttpStatus.NOT_FOUND,"Not founded"),
+    INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, "패스워드가 잘못되었습니다."),
+    INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "잘못된 토큰입니다."),
+    INVALID_PERMISSION(HttpStatus.UNAUTHORIZED, "사용자가 권한이 없습니다."),
+    POST_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 포스트가 없습니다."),
+    DATABASE_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "DB에러");
 
     //커스텀용 변수
     private HttpStatus status;
